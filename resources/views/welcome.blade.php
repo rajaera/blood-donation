755b2ebe708c0f6a9f1bb32ec67328a3258b9639
Eranga Perera <rajaera@gmail.com>
2022-10-24 21:45:54 +0530

    adding basic nav bar to the welcome.blade

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,6 +13,15 @@
         
     </head>
     <body class="antialiased">
-        <h1>Blood Donation Charity</h1>
+        <nav class="navbar navbar-light bg-light justify-content-between">
+            <a class="navbar-brand">Blood Donation Charity</a>
+            <form class="form-inline">              
+              <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Logout</button>
+            </form>
+          </nav>
+        <div class="container-fluid">
+            <h1>Welcome!</h1>
+        </div>
+        
     </body>
 </html>
