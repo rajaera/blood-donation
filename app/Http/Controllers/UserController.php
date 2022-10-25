c9fb1d8b70fafcc9ac9f9724b2f4ed0b4f12b84b
Eranga Perera <rajaera@gmail.com>
2022-10-25 22:28:10 +0530

    adding user list @ index

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 
 class UserController extends Controller
@@ -23,6 +24,6 @@ class UserController extends Controller
      */
     public function index()
     {
-        return view('user.index');
+        return view('user.index', ['users' => User::orderBy('name')->get()]);
     }
 }
--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -4,7 +4,34 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
-            <h1>User List</h1>
+            <div class="card">
+                <div class="card-header">{{ __('User List') }}</div>
+
+                <div class="card-body">
+                    <table class="table">
+                        <thead>
+                          <tr>
+                            <th scope="col">#</th>
+                            <th scope="col">Username</th>
+                            <th scope="col">Email</th>
+                            <th scope="col">Created At</th>
+                          </tr>
+                        </thead>
+                        <tbody>
+                            @foreach ($users as $user)
+                            <tr>
+                                <th scope="row">1</th>
+                                <td>{{ $user->name }}</td>
+                                <td>{{ $user->email }}</td>
+                                <td>{{ $user->created_at }}</td>
+                              </tr>
+                            @endforeach
+                          
+                          
+                        </tbody>
+                      </table>
+                </div>
+            </div>
         </div>
     </div>
 </div>
