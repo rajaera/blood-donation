fae222b91c2f9610e4142f4996bc837992f7ecf2
Eranga Perera <rajaera@gmail.com>
2022-10-25 22:35:02 +0530

    import bootstrap icons @ resource app.scss and run dev

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -5,7 +5,7 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('User List') }}</div>
+                <div class="card-header">{{ __('User List') }} <i class="bi-alarm"></i></div>
 
                 <div class="card-body">
                     <table class="table">
